b0efd64b1e65cf95ed2277d6b24911a72d6e3b6d
Jan Molak <jan.molak@gmail.com>
2013-01-04 17:27:32 +0000

    Dev vm can be provisioned with apache 2; Refactored only_existing_sources function so it correctly handles puppet:/// source uris. Note: it still doesn't work well with  in the uri so I need to find a fix for that

--- lib/puppet/parser/functions/only_existing_sources.rb
+++ lib/puppet/parser/functions/only_existing_sources.rb
@@ -1,10 +1,18 @@
 module Puppet::Parser::Functions
-  newfunction(:only_existing_sources, :type => :rvalue) do | args |
+  newfunction(:only_existing_sources, :type => :rvalue) do |args|
     sources = []
 
     args.flatten.each do | source |
-      path = Pathname.new(source)
-      sources << source unless path.absolute? and not path.exist?
+      unless Puppet::Util.absolute_path?(source)
+        uri = URI.parse(URI.escape(source))
+        raise Puppet::ParseError, ("Cannot use relative URLs '#{source}'") unless uri.absolute?
+        raise Puppet::ParseError, ("Cannot use opaque URLs '#{source}'") unless uri.hierarchical?
+        raise Puppet::ParseError, ("Cannot use URLs of type '#{uri.scheme}' as source for fileserving") unless %w{puppet}.include?(uri.scheme)
+
+        sources << source unless Puppet::FileServing::Content.indirection.find(source).nil?
+      else
+        sources << source if Pathname.new(source).exist?
+      end
     end
 
     sources
